fdf66e5cf0c2fb0e1bcc0e10b3d4448a6d3d9b37
Christian Marie <christian@ponies.io>
2014-02-26 08:39:41 +1100

    Handle timeout to ZMQ in chevalier thread also, this timeout is longer than the one in the caller and it's purpose is to recover from a possibly deadlocked or inconsistent chevalier daemon.

--- src/Chevalier.hs
+++ src/Chevalier.hs
@@ -4,13 +4,13 @@ module Chevalier where
 import           Control.Applicative
 import           Control.Concurrent
 import           Control.Exception
-import           Control.Monad
 import           Data.Monoid            ((<>))
 import           Data.ProtocolBuffers   hiding (field)
 import           Data.Serialize
 import qualified Data.Text.Lazy.Builder as LazyBuilder
 import           System.ZMQ4            hiding (source)
 import           Types
+import           System.Timeout               (timeout)
 
 -- | Chevalier communication thread, reads SourceQuery requsts from an mvar and
 -- replies over the included mvar.
@@ -18,14 +18,29 @@ chevalier :: String -> MVar SourceQuery -> IO ()
 chevalier chevalier_url query_mvar =
     withContext $ \c -> withSocket c Req $ \s -> do
         connect s chevalier_url
-        forever $ do
-            query <- takeMVar query_mvar
-            result <- try $ do
-                send s [] $ encodeChevalierRequest $ buildChevalierRequest query
-                response <- decodeChevalierResponse <$> receive s
-                either error (return . buildSources) response
-            putMVar (queryResponse query) result
+        loop s
   where
+    loop s = do
+        query <- takeMVar query_mvar
+
+        result <- timeout chevalierTimeout . try $ do
+            send s [] $ encodeChevalierRequest $ buildChevalierRequest query
+            response <- decodeChevalierResponse <$> receive s
+            either error (return . buildSources) response
+
+        case result of 
+            -- Timeout has triggered, we assume that the chevalier daemon has
+            -- deadlocked or restarted so we need a new connection.
+            --
+            -- The code that is waiting for a response over the response mvar
+            -- has a timeout also, which is shorter and has already triggered.
+            Nothing -> chevalier chevalier_url query_mvar
+            -- Otherwise we have a response, error or not.
+            Just result' -> putMVar (sourceResponse query) result'
+
+
+    chevalierTimeout = 60000000 -- 60s
+
     encodeChevalierRequest = runPut . encodeMessage
 
     decodeChevalierResponse = runGet decodeMessage
